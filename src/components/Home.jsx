281704c265acfdd38496a11ddeaf3afb4349e2a7
Joshua Kiley <joshua.kiley.dev@gmail.com>
2020-01-07 16:49:48 -0500

    began home page blog

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,18 +14,28 @@ class Home extends Component {
             <h2>SOFTWARE ENGINEER</h2>
           </div>
         </section>
-        <section>
+        <section className="home-intro">
           <div>
-            <h1>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem et
-              nihil sapiente! Veritatis perspiciatis voluptatibus rerum saepe
-              blanditiis, quidem enim ullam nam, ratione aperiam laudantium
-              velit soluta nobis, possimus temporibus! Lorem ipsum dolor sit
-              amet, consectetur adipisicing elit. Adipisci doloribus repellat,
-              at autem sapiente consectetur! Temporibus veritatis, tenetur
-              accusantium asperiores vel in, recusandae tempora iure non harum
-              officia, aspernatur facilis!
-            </h1>
+            <h1>Just get on the boat.</h1>
+            <h2>
+              When I was 17 years old I learned one of the most important
+              lessons of my life.
+            </h2>
+            <div className="paragraphs">
+              <p>
+                Summer of 2004 I was looking for a job. I had a job, a part-time
+                job at a restaurant, but I wanted a full-time job for the
+                summer. I wanted to know what it felt like to have something
+                other than my studies take up my day to day, and something other
+                than the lull of other summers. This summer I found just that,
+                and I found a piece of wisdom that I unknowingly kept with me
+                for a long time. It wasn't until much later in life when I
+                relaized that this philosophy had helped me navigate through
+                many predicaments.
+              </p>
+              <hr></hr>
+              <p></p>
+            </div>
           </div>
         </section>
       </main>
